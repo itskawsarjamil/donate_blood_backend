79daf85b8ac4b913615f1158b560ccabc783d297
Kawsar Jamil <Kawsarjamil726@gmail.com>
2025-04-21 14:42:43 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import { Server } from "http";
+import app from "./app";
+
+let server: Server;
+async function main() {
+  try {
+    server = app.listen(5000, () => {
+      console.log(`Example app listening on port ${5000}`);
+    });
+  } catch (er) {
+    console.log(er);
+  }
+}
+
+main();
+
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (err: Error) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+});
